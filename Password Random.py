2b5655ba4c60d5ba9e037501a0b303e6b3656752
Mahdi Yaganeh <mahdiyaganeh91@gmail.com>
2022-12-04 19:35:42 +0330

    Delete Password Random.py

--- Password Random.py
+++
@@ -1,5 +0,0 @@
-import random
-password = int(input("Enter the lengh of password:"))
-s="abcdefghijklmnopqrstuvwxyz0123456789ABCEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?"
-p="".join(random.sample(s,password))
-print(p)
